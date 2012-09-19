194a46e9e096715d2b1f6a6040e4d56bd8969136
vedantb <vedantbhatt@gmail.com>
2012-09-19 01:10:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviemania::Application.config.secret_token = '0413f0e3a43de4eb6fe184c218afb1eea31e444857f8873a9250e9a5b7ce76012d051814a6d2857581fd0620b967bd6fa30afcd1cb8a6a7657eb1292942fd2ec'
